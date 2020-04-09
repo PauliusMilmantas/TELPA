606820ca7275e96d44acbe50aa61c0482e4892c9
Arnas Kelpša <arnastk@gmail.com>
2020-04-09 21:48:34 +0300

    Pridėtas Entity Framework, prisijungimas prie DB ir sukurtas User entity

--- TELPA/Controllers/HomeController.cs
+++ TELPA/Controllers/HomeController.cs
@@ -3,13 +3,21 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using TELPA.Models;
 
 namespace TELPA.Controllers
 {
     public class HomeController : Controller
     {
+        ModelContext db = new ModelContext();
         public ActionResult Index()
         {
+            User user = new User();
+            user.name = "Petras";
+            user.last_name = "Petraitis";
+
+            db.Users.Add(user);
+            db.SaveChanges();
             return View();
         }
 
---
+++ TELPA/Models/ModelContext.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Web;
+
+namespace TELPA.Models
+{
+    public class ModelContext : DbContext
+    {
+        public ModelContext()
+            : base("name=TELPA_DB")
+        { }
+
+        public virtual DbSet<User> Users { get; set; }
+    }
+}
