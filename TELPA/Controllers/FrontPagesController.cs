2eab8300f263392cf2cb28b265c4ac9d91e0aab0
=pauliusmilmantas <=pauliusmilmantas@gmail.com>
2020-04-20 22:18:25 +0300

    Front puslapis

---
+++ TELPA/Controllers/FrontPagesController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace TELPA.Controllers
+{
+    public class FrontPagesController : Controller
+    {
+        // GET: FrontPages
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
