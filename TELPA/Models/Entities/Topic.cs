7c81bdcd2778b2b15b852cded2c08da449c86bff
=pauliusmilmantas <=pauliusmilmantas@gmail.com>
2020-04-22 12:05:54 +0300

    New entities

---
+++ TELPA/Models/Entities/Topic.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace TELPA.Models.Entities
+{
+    public class Topic
+    {
+        [Key]
+        public int topic_id { get; set; }
+
+        public string name { get; set; }
+    }
+}
