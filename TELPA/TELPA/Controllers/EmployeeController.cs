66a5e29534cf5aa8adaf4678871e5e0a800c4ca9
Arnas Kelpša <arnastk@gmail.com>
2020-05-18 23:54:18 +0300

    Employee controller get/post/put

--- TELPA/TELPA/Controllers/EmployeeController.cs
+++ TELPA/TELPA/Controllers/EmployeeController.cs
@@ -1,17 +1,71 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Net.Http;
-using System.Threading.Tasks;
-using Microsoft.AspNetCore.Mvc;
+﻿using Microsoft.AspNetCore.Mvc;
+using TELPA.Data;
 using TELPA.Models;
 
 namespace TELPA.Controllers
 {
+    [Route("api/employee")]
     public class EmployeeController : Controller
     {
-        // GET: /Employee/
-        public ActionResult Index()
+        private ApplicationDbContext db;
+        public EmployeeController(ApplicationDbContext db)
+        {
+            this.db = db;
+        }
+
+        [HttpGet("ping")]
+        public IActionResult ping()
+        {
+            return Ok("System online");
+        }
+
+        [HttpGet]
+        [Route("{id}")]
+        public IActionResult getEmployee(string id)
+        {
+            return Json(db.Employees.Find(id));
+        }
+
+        [HttpPost("create")]
+        public IActionResult createEmployee(Employee employee)
+        {
+            this.db.Employees.Add(employee);
+            this.db.SaveChanges();
+            return Ok("Employee created");
+        }
+
+        [HttpPost("update")]
+        public IActionResult updateEmployee(Employee employee)
+        {
+            this.db.Employees.Update(employee);
+            this.db.SaveChanges();
+            return Ok("Employee updated");
+        }
+
+        [HttpGet("test/1")]
+        public IActionResult testCreate()
+        {
+            Employee emp = new Employee();
+            emp.UserId = "3";
+            emp.Name = "Ignas";
+            emp.Version = 0;
+
+            return createEmployee(emp);
+        }
+
+        [HttpGet("test/2")]
+        public IActionResult testUpdate()
+        {
+            Employee emp = this.db.Employees.Find("2");
+            emp.Role = "New role";
+            emp.Name = emp.Name + "2";
+            
+            return updateEmployee(emp);
+        }
+
+
+
+        /*public ActionResult Index()
         {
             IEnumerable<Employee> employees = null;
              
@@ -37,6 +91,7 @@ namespace TELPA.Controllers
             }
 
             return View(employees);
-        }
+        }*/
+
     }
 }
