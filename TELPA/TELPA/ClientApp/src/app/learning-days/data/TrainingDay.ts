39a5a46c64b2499fe96a0570ee94517587d315c0
=pauliusmilmantas <=pauliusmilmantas@gmail.com>
2020-05-20 15:31:22 +0300

    Dynamic learning days list

---
+++ TELPA/TELPA/ClientApp/src/app/learning-days/data/TrainingDay.ts
@@ -0,0 +1,4 @@
+export interface LearningDay {
+  date: String;
+  topic: String;
+}
---
+++ TELPA/TELPA/ClientApp/src/app/learning-days/data/mock_data.ts
@@ -0,0 +1,10 @@
+import { LearningDay } from './TrainingDay';
+
+export const TrainingDayList: LearningDay[] = [
+  { date: "2020-05-20", topic: "Cuda1" },
+  { date: "2020-05-20", topic: "Cuda2" },
+  { date: "2020-05-20", topic: "Cuda3" },
+  { date: "2020-05-20", topic: "Cuda4" },
+  { date: "2020-05-20", topic: "Cuda5" },
+  { date: "2020-05-20", topic: "Cuda6" }
+]
