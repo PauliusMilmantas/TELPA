5c47185da32c07663c70f82f6ca060634c67b068
Efkinatorius <36881430+Efkinatorius@users.noreply.github.com>
2020-05-21 11:48:55 +0300

    cannot get

--- TELPA/TELPA/ClientApp/src/app/employee-management/employee-management.component.ts
+++ TELPA/TELPA/ClientApp/src/app/employee-management/employee-management.component.ts
@@ -1,6 +1,9 @@
 import { Component, OnInit } from '@angular/core';
 import { EmployeeDataList } from './data/mock_employee_data';
 import { EmployeeData } from './data/data';
+import { EmployeeServiceService } from './service/employee-service.service';
+import { Observable } from 'rxjs';
+
 
 @Component({
   selector: 'app-employee-management',
@@ -9,9 +12,15 @@ import { EmployeeData } from './data/data';
 })
 export class EmployeeManagementComponent implements OnInit {
 
-  constructor() { }
+  characters: Observable<EmployeeData[]>;
+  columns: string[];
+
+  constructor(private atService: EmployeeServiceService) { }
 
   ngOnInit() {
+    this.columns = this.atService.getColumns();
+
+    this.characters = this.atService.getEmployees();
   }
 
   selectTeamBtnClick(event) {
