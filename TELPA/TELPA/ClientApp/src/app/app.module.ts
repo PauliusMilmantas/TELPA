4bbffa583702a21e2ca1ece427446b806c4096f4
=pauliusmilmantas <=pauliusmilmantas@gmail.com>
2020-05-22 19:44:00 +0300

    Auth fix

--- TELPA/TELPA/ClientApp/src/app/app.module.ts
+++ TELPA/TELPA/ClientApp/src/app/app.module.ts
@@ -10,9 +10,6 @@ import { NavMenuComponent } from './nav-menu/nav-menu.component';
 import { HomeComponent } from './home/home.component';
 import { CounterComponent } from './counter/counter.component';
 import { FetchDataComponent } from './fetch-data/fetch-data.component';
-import { ApiAuthorizationModule } from 'src/api-authorization/api-authorization.module';
-import { AuthorizeGuard } from 'src/api-authorization/authorize.guard';
-import { AuthorizeInterceptor } from 'src/api-authorization/authorize.interceptor';
 import { FrontPageComponent } from './front-page/front-page.component';
 import { CalendarWindowComponent } from './calendar-window/calendar-window.component';
 import { HomePageComponent } from './home-page/home-page.component';
@@ -46,12 +43,11 @@ import { ManagerHomePageComponent } from './manager-home-page/manager-home-page.
     BrowserModule.withServerTransition({ appId: 'ng-cli-universal' }),
     HttpClientModule,
     FormsModule,
-    ApiAuthorizationModule,
     ModalModule,
     RouterModule.forRoot([
           { path: '', component: FrontPageComponent },
           { path: 'counter', component: CounterComponent },
-          { path: 'fetch-data', component: FetchDataComponent, canActivate: [AuthorizeGuard] },
+          { path: 'fetch-data', component: FetchDataComponent },
           { path: 'calendar', component: CalendarWindowComponent },
           { path: 'homepage', component: HomePageComponent },
           { path: 'register', component: RegistrationFormComponent },
@@ -60,7 +56,6 @@ import { ManagerHomePageComponent } from './manager-home-page/manager-home-page.
     ])
   ],
   providers: [
-    { provide: HTTP_INTERCEPTORS, useClass: AuthorizeInterceptor, multi: true }
   ],
   bootstrap: [AppComponent]
 })
