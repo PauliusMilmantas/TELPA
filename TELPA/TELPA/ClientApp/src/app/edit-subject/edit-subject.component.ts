b400a0fbf38b5b41770b7bd0e3fe0c5dc1632752
Tadas Gricius <griciustadas@gmail.com>
2020-05-22 16:10:59 +0200

    Added static edit subject page

--- TELPA/TELPA/ClientApp/src/app/edit-subject/edit-subject.component.ts
+++ TELPA/TELPA/ClientApp/src/app/edit-subject/edit-subject.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { SubjectData } from '../add-subject/data/data';
+
 
 @Component({
   selector: 'app-edit-subject',
@@ -7,9 +9,40 @@ import { Component, OnInit } from '@angular/core';
 })
 export class EditSubjectComponent implements OnInit {
 
+  subjectToAdd = new SubjectData(1, 2, '', '', ['']);
+  hideForm = true;
+  isEmpty = [false];
+ 
+
   constructor() { }
 
   ngOnInit() {
+    this.hideForm = true;
   }
 
+  onSubmit() { }
+
+  onSubjectChange(value) {
+    if (value == "(None)") {
+      this.hideForm = true;
+    }
+    else {
+      this.hideForm = false;
+    }
+  }
+
+  onLinkChange(value, place) {
+    this.subjectToAdd.links[place] = value;
+    if (place == (this.subjectToAdd.links.length - 1) && this.subjectToAdd.links[place].length != 0) {
+      this.subjectToAdd.links.push('');
+      this.isEmpty.push(false);
+    }
+    else if (place != (this.subjectToAdd.links.length - 1) && this.subjectToAdd.links[place].length == 0) {
+      this.isEmpty[place] = true;
+    }
+  }
+
+  trackByFn(index: any, item: any) {
+    return index;
+  }
 }
