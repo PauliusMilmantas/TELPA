b5e8ead459193fbcfa3613b4c423a43df9904e2b
Tadas Gricius <griciustadas@gmail.com>
2020-05-24 18:25:56 +0200

    Testing API

--- TELPA/TELPA/ClientApp/src/app/topic-add/topic-add.component.ts
+++ TELPA/TELPA/ClientApp/src/app/topic-add/topic-add.component.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { TopicData } from './data/data';
+import { HttpClient } from '@angular/common/http';
+
 @Component({
   selector: 'app-topic-add',
   templateUrl: './topic-add.component.html',
@@ -7,17 +9,33 @@ import { TopicData } from './data/data';
 })
 export class TopicAddComponent implements OnInit {
 
+  topics: TopicData[];
+
   topicToAdd = new TopicData(1, 2, '', '', ['']);
   isEmpty = [false];
 
-  constructor() { }
+  constructor(private httpClient: HttpClient) { }
 
   ngOnInit() {
+    this.getBackendData();
   }
 
-  onSubmit() {  }
 
-  onLinkChange(value, place) {
+  onSubmit() { }
+
+  // Requesting data from API
+  getBackendData() {
+    var baseURL = location.origin;
+    this.httpClient.get(baseURL + '/api/topic/get/all').subscribe(
+      data => {
+        for (var i = 0; i < Object.keys(data).length; i++) {
+          console.log(data[i]);
+        }
+      }
+    )
+  }
+
+ /* onLinkChange(value, place) {
     this.topicToAdd.links[place] = value;
     if (place == (this.topicToAdd.links.length - 1) && this.topicToAdd.links[place].length != 0) {
       this.topicToAdd.links.push('');
@@ -30,5 +48,5 @@ export class TopicAddComponent implements OnInit {
 
   trackByFn(index: any, item: any) {
    return index;
-}
+  }*/
 }
