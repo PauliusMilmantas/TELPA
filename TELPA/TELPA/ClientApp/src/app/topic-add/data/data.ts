ecd8271f3cf8ea06985c73105b9f2bba1f392d1b
Tadas Gricius <griciustadas@gmail.com>
2020-05-24 21:43:23 +0200

    Added dynamic topic selector for add topic page

--- TELPA/TELPA/ClientApp/src/app/topic-add/data/data.ts
+++ TELPA/TELPA/ClientApp/src/app/topic-add/data/data.ts
@@ -1,9 +1,6 @@
-export class TopicData {
-  constructor(
-    public id: number,
-    public parentId: number,
-    public name: string,
-    public description: string,
-    public links: string[]
-  ) { }
+export interface TopicData {
+  id: number,
+  name: string,
+  description: string,
+  parentTopicId: number,
 }
--- TELPA/TELPA/ClientApp/src/app/topic-add/topic-add.component.ts
+++ TELPA/TELPA/ClientApp/src/app/topic-add/topic-add.component.ts
@@ -1,5 +1,4 @@
 import { Component, OnInit } from '@angular/core';
-import { TopicData } from './data/data';
 import { HttpClient } from '@angular/common/http';
 
 @Component({
@@ -9,9 +8,9 @@ import { HttpClient } from '@angular/common/http';
 })
 export class TopicAddComponent implements OnInit {
 
-  topics: TopicData[];
+  private apiTopics = [];
+  topics = [];
 
-  topicToAdd = new TopicData(1, 2, '', '', ['']);
   isEmpty = [false];
 
   constructor(private httpClient: HttpClient) { }
@@ -28,12 +27,63 @@ export class TopicAddComponent implements OnInit {
     var baseURL = location.origin;
     this.httpClient.get(baseURL + '/api/topic/get/all').subscribe(
       data => {
-        for (var i = 0; i < Object.keys(data).length; i++) {
-          console.log(data[i]);
+        for (let i = 0; i < Object.keys(data).length; i++) {
+          this.apiTopics.push(
+            {
+              'id': data[i]['id'],
+              'name': data[i]['name'],
+              'description': data[i]['description'],
+              'parentTopicId': data[i]['parentTopicId']
+            }
+          );
         }
       }
-    )
+    ).add(() => {
+      this.parseTopics();
+    });
   }
+
+  parseTopics() {
+
+    let index = 1;
+    for (let i = 0; i < this.apiTopics.length; i++) {
+      if (this.apiTopics[i]['parentTopicId'] == null) {
+        this.topics.push(
+          {
+            'id': this.apiTopics[i]['id'],
+            'index': index,
+            'name': this.apiTopics[i]['name'],
+            'subtopicAmount': 0,
+            'level': 0
+          }
+        );
+        this.apiTopics.splice(i, 1);
+        index++;
+      }
+    }
+
+
+    while (this.apiTopics.length != 0) {
+      for (let i = 0; i < this.topics.length; i++) {
+        for (let j = 0; j < this.apiTopics.length; j++) {
+          if (this.topics[i]['id'] == this.apiTopics[j]['parentTopicId']) {
+            this.topics[i]['subtopicAmount']++;
+            this.topics.splice(i + this.topics[i]['subtopicAmount'], 0,
+              {
+                'id': this.apiTopics[j]['id'],
+                'index': this.topics[i]['index'] + '.' + this.topics[i]['subtopicAmount'],
+                'name': this.apiTopics[j]['name'],
+                'subtopicAmount': 0,
+                'level': this.topics[i]['level'] + 1
+              }
+            );
+            this.apiTopics.splice(j, 1);
+          }
+        }
+      }
+    }
+  }
+
 
  /* onLinkChange(value, place) {
     this.topicToAdd.links[place] = value;
