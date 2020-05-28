9c65a5f81aa3f1665cf37387539a2b69ec53bae4
Efkinatorius <36881430+Efkinatorius@users.noreply.github.com>
2020-05-28 10:48:05 +0300

    started to do email service

---
+++ TELPA/TELPA/Components/EmailService.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace TELPA.Components
+{
+    public class EmailService : IEmailService
+    {
+        public string SendEmail(string email)
+        {
+            return "k";
+        }
+    }
+}
